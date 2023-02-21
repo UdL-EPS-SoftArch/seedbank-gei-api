e477abd47691f7dd8514e58072813f6ff8d619b0
FerranAD <farandomingo@gmail.com>
2023-02-21 18:03:42 +0100

    refactor(donation): make donation class inmutable

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -20,37 +20,22 @@ public class Donation {
 //    @JoinColumn(name = "batch_id")
 //    private Batch batch;
 
-    public Donation() {
-    }
-
 //    public Donation(Donor donor, Batch batch) {
 //        this.donor = donor;
 //        this.batch = batch;
 //    }
 
-    // getters and setters
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
 //    public Donor getDonor() {
 //        return donor;
 //    }
 //
-//    public void setDonor(Donor donor) {
-//        this.donor = donor;
-//    }
-//
 //    public Batch getBatch() {
 //        return batch;
 //    }
 //
-//    public void setBatch(Batch batch) {
-//        this.batch = batch;
-//    }
 
 }
