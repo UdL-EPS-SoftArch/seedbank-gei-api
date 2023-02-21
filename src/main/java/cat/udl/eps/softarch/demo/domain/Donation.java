c1bf7f44e3a5430a43f682654d585da888d86f6c
FerranAD <farandomingo@gmail.com>
2023-02-21 18:26:46 +0100

    refactor(donation): remove unneeded getters

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -1,12 +1,15 @@
 package cat.udl.eps.softarch.demo.domain;
 
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 public class Donation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,11 +20,11 @@ public class Donation {
 //    @ManyToOne
 //    @JoinColumn(name = "donor_id")
 //    private Donor donor;
-
+//
 //    @ManyToOne
 //    @JoinColumn(name = "batch_id")
 //    private Batch batch;
-
+//
 //    public Donation(Donor donor, Batch batch) {
 //        this.donor = donor;
 //        this.batch = batch;
@@ -30,14 +33,5 @@ public class Donation {
     public Long getId() {
         return id;
     }
-
-//    public Donor getDonor() {
-//        return donor;
-//    }
-//
-//    public Batch getBatch() {
-//        return batch;
-//    }
-//
 
 }
