f7fc14dca3560f9be0edfd21bf3609cd3762529b
FerranAD <farandomingo@gmail.com>
2023-02-21 17:56:24 +0100

    feat(donation): add annotations for spirng

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -1,6 +1,14 @@
 package cat.udl.eps.softarch.demo.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
