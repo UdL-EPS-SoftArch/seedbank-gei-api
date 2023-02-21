8876d026ce20c8865aed7d279956c5e9662e0a20
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-02-21 18:01:28 +0100

    feat(donor): add donations relationship to donors

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donor.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donor.java
@@ -1,5 +1,31 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+
+@Entity
+@DiscriminatorValue("donor")
 public class Donor extends User {
+
+    @OneToMany(mappedBy = "donor")
+    private Collection<Donation> donations;
+
+    // TODO: Delete this after the donation pull request is done
+    @Entity
+    class Donation {
+        @Id
+        private Long id;
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
 
 }
