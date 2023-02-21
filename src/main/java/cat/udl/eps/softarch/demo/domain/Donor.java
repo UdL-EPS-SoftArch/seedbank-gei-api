bbb4b324fd1e85ff75ac3cd5c5512b720df230d8
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-02-21 18:06:31 +0100

    feat(donor): add immutable donations getter
    Be aware of the immutability of the list since, if we don't return a
    deep copy, the user could change the contents of the list

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donor.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donor.java
@@ -1,10 +1,14 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @DiscriminatorValue("donor")
@@ -12,6 +16,11 @@ public class Donor extends User {
 
     @OneToMany(mappedBy = "donor")
     private Collection<Donation> donations;
+
+
+    public Collection<Donation> getDonations() {
+        return Collections.unmodifiableCollection(this.donations);
+    }
 
     // TODO: Delete this after the donation pull request is done
     @Entity
