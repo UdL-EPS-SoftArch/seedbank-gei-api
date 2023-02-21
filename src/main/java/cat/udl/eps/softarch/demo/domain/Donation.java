0cf3c5e19bf882c314abf59516169a480f1aac9d
FerranAD <farandomingo@gmail.com>
2023-02-21 18:01:31 +0100

    feat(donation): add constructors, getters and setters

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -1,7 +1,10 @@
 package cat.udl.eps.softarch.demo.domain;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Donation {
@@ -16,5 +19,38 @@ public class Donation {
 //    @ManyToOne
 //    @JoinColumn(name = "batch_id")
 //    private Batch batch;
+
+    public Donation() {
+    }
+
+//    public Donation(Donor donor, Batch batch) {
+//        this.donor = donor;
+//        this.batch = batch;
+//    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+//    public Donor getDonor() {
+//        return donor;
+//    }
+//
+//    public void setDonor(Donor donor) {
+//        this.donor = donor;
+//    }
+//
+//    public Batch getBatch() {
+//        return batch;
+//    }
+//
+//    public void setBatch(Batch batch) {
+//        this.batch = batch;
+//    }
 
 }
