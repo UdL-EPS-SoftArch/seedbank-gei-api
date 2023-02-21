f398fa2755a4592987b3131278877edf9ecf1820
FerranAD <farandomingo@gmail.com>
2023-02-21 17:58:23 +0100

    feat(donation): add template for entity relations #2

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -1,14 +1,20 @@
 package cat.udl.eps.softarch.demo.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Donation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "donor_id")
+//    private Donor donor;
+
+//    @ManyToOne
+//    @JoinColumn(name = "batch_id")
+//    private Batch batch;
+
 }
