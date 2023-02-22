c3b18e8b2f2434d25a9882cbf629e7bafad55d4b
MarioMontanuy <mariobnf58@gmail.com>
2023-02-22 18:11:55 +0100

    Adds deleteBatchById Method

--- src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
@@ -4,9 +4,12 @@ import cat.udl.eps.softarch.demo.domain.Take;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.net.http.HttpResponse;
+
 public interface TakeRepository extends PagingAndSortingRepository<Take, String> {
 
     //This is not void, when merge uncomment Batch
     /*Batch*/ void findBatchById(@Param("id") Integer id);
 
+    HttpResponse <String> deleteBatchById(@Param("id") Integer id);
 }
