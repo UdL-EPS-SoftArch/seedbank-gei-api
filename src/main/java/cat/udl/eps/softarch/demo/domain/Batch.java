836f3727c443dd79dc6f7a9ed36d27a2e5860c59
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-02-22 18:07:20 +0100

    feat: add all class members on batch

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -1,11 +1,16 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 @Entity
 @Data
@@ -13,4 +18,8 @@ public class Batch {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private long id;
+    private Integer amount;
+    private BigDecimal weight;
+    private String location;
+    private ZonedDateTime date;
 }
