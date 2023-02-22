f79dd5773177ea7f1aefb25787b442c80f51610d
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-02-22 18:11:52 +0100

    feat: add constraint on batch

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -7,8 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
@@ -18,8 +17,17 @@ public class Batch {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private long id;
+
+
+    @NotNull
+    @Min(value = 1)
     private Integer amount;
+
+    @NotNull
+    @DecimalMin(value = "0")
     private BigDecimal weight;
+    @NotBlank
+    @Length(min = 2, max = 30)
     private String location;
-    private ZonedDateTime date;
+    private ZonedDateTime date = ZonedDateTime.now()
 }
