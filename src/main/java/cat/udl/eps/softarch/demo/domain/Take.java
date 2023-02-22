395baaff8bfa5bd12e0f8cabdcef648d47a77b6e
sys-walker <pere.rollon@gmail.com>
2023-02-22 17:59:58 +0100

    Add date to take

--- src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
@@ -3,11 +3,17 @@ package cat.udl.eps.softarch.demo.domain;
 import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Entity
 @Data
 public class Take /*extends Batch */{
     @Id
     private Integer id;
+
+    @NotBlank
+    private ZonedDateTime date;
 
 }
