25b0601a08e4130f80055bb608964c9a0f3fbeba
MarioMontanuy <mariobnf58@gmail.com>
2023-02-22 18:05:17 +0100

    Adds GeneratedValue Auto

--- src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
@@ -2,15 +2,17 @@ package cat.udl.eps.softarch.demo.domain;
 
 import lombok.Data;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import java.time.ZonedDateTime;
-import java.util.Date;
 
 @Entity
 @Data
 public class Take /*extends Batch */{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @NotBlank
