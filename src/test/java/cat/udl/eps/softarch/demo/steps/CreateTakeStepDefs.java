7b591ebdc1333c35564dfa678812734faae9a86c
Oscars35 <oscar13785@gmail.com>
2023-02-23 16:28:22 +0100

    Adds Given Scenario to CreateTakeStepDefs.java

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateTakeStepDefs.java
@@ -1,7 +1,10 @@
 package cat.udl.eps.softarch.demo.steps;
 
+import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
 import io.cucumber.java.en.Given;
+
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CreateTakeStepDefs {
@@ -11,7 +14,8 @@ public class CreateTakeStepDefs {
     @Autowired
     private TakeRepository takeRepository;
 
-//    @Given("^The response code is (\\d+)$")
-//    public void
-
+    @Given("^There is no Take available with id (\\d+)$")
+    public void thereIsNoTakeAvailableWithId(Integer id) {
+        Assert.assertNull(this.takeRepository.findById(id));
+    }
 }
