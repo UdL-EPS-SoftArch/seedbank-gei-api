da12aa92b59392c7276127c8390cc10310631654
Oscars35 <oscar13785@gmail.com>
2023-02-23 17:06:45 +0100

    Adds RepositoryRestResource to TakeRepository

--- src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
@@ -3,7 +3,9 @@ package cat.udl.eps.softarch.demo.repository;
 import cat.udl.eps.softarch.demo.domain.Take;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TakeRepository extends PagingAndSortingRepository<Take, String> {
 
     //This is not void, when merge uncomment Batch
@@ -11,4 +13,5 @@ public interface TakeRepository extends PagingAndSortingRepository<Take, String>
     Take findTakeTakenByBatchId(@Param("id") Integer batchId);
     Take findById(@Param("id") Integer takeId);
     Boolean existsById(@Param("id") Integer takeId);
+    
 }
