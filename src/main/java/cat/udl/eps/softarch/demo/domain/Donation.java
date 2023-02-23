a99b70d7804a1edc3fe62ddd05d8a270c666e435
Roberto García <roberto@rhizomik.net>
2023-02-23 11:31:35 +0100

    Remove inherited field id

--- src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Donation.java
@@ -1,20 +1,15 @@
 package cat.udl.eps.softarch.demo.domain;
 
-
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
-
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Donation extends Batch {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
@@ -23,11 +18,4 @@ public class Donation extends Batch {
     @OneToOne
     @JsonIdentityReference(alwaysAsId = true)
     private Take takenBy;
-
-    public Donation(Donor donor, Take takenBy) {
-        this.donor = donor;
-        this.takenBy = takenBy;
-    }
-
-    protected Donation() {}
 }
