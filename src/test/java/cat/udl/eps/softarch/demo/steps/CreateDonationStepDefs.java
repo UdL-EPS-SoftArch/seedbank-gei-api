4fdf43c9ba2373f15b8ff27343b7418c23e0e201
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-02-23 16:53:45 +0100

    refactor: rename feature and steps definitions for better understanding

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateDonationStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateDonationStepDefs.java
@@ -1,13 +1,15 @@
 package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Donor;
+import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.repository.DonationRepository;
 import cat.udl.eps.softarch.demo.repository.UserRepository;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 
-import static org.springframework.http.RequestEntity.post;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 public class RegisterDonationStepDefs {
     @Autowired
@@ -17,12 +19,37 @@ public class RegisterDonationStepDefs {
     @Autowired
     private UserRepository userRepository;
 
+    private Donor donor;
+    private Take take;
+
     @And("There is a valid Donor")
     public void thereIsAValidDonor() {
+        donor = createValidDonor();
+        Take take = createValidTake();
+    }
+
+    @And("There is a valid Take")
+    public void thereIsAValidTake() {
+        take = createValidTake();
+    }
+
+    @When()
+
+
+    private Donor createValidDonor() {
         Donor donor = new Donor();
         donor.setUsername("donor");
         donor.setPassword("donor");
         donor.setEmail("donor@donor.donor");
-        userRepository.save(donor);
+        return donor;
+    }
+
+    private Take createValidTake() {
+        Take take = new Take();
+        take.setAmount(6);
+        take.setWeight(BigDecimal.TEN);
+        take.setLocation("Barcelona");
+        take.setDate(ZonedDateTime.now());
+        return take;
     }
 }
