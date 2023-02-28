e8a46a990f4523f35260bf3c9c98a4714ae5fc74
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-02-28 18:34:45 +0100

    feat: add seed implementation
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/repository/SeedRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/SeedRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface SeedRepository extends PagingAndSortingRepository<Seed, String> {
+public interface SeedRepository extends PagingAndSortingRepository<Seed, Long> {
 }
