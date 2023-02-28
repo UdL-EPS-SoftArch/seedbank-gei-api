3ccfef91f2281f68796fe549d5859b2ec22879a9
MarioMontanuy <mariobnf58@gmail.com>
2023-02-28 18:46:01 +0100

    Adds takeDate in Take

--- src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Take.java
@@ -4,12 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.time.ZonedDateTime;
 
 @Entity
@@ -17,6 +12,6 @@ import java.time.ZonedDateTime;
 @EqualsAndHashCode(callSuper = true)
 public class Take extends Batch {
     @NotNull
-    private ZonedDateTime date;
+    private ZonedDateTime takeDate;
 
 }
