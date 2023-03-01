d2793853e5b5838aa831a64fe706134ca0af0abc
Oscar <oscar13785@gmail.com>
2023-03-01 18:08:18 +0100

    Adds autowired variables in GetTakeStepDefs

--- src/test/java/cat/udl/eps/softarch/demo/steps/GetTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/GetTakeStepDefs.java
@@ -1,5 +1,14 @@
 package cat.udl.eps.softarch.demo.steps;
 
+import cat.udl.eps.softarch.demo.repository.TakeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class GetTakeStepDefs {
 
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Autowired
+    private TakeRepository takeRepository;
 }
