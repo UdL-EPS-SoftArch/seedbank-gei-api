76cb1c083730f6873b28815d577d156b84f85845
MarioMontanuy <mariobnf58@gmail.com>
2023-03-01 18:15:47 +0100

    Adds thereIsTakeAvailableWithId checked in TakeRepository

--- src/test/java/cat/udl/eps/softarch/demo/steps/DeleteTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/DeleteTakeStepDefs.java
@@ -2,11 +2,14 @@ package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -29,5 +32,10 @@ public class DeleteTakeStepDefs {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(stepDefs.mapper.writeValueAsString(take))
         );
+    }
+
+    @Given("^There is Take available with id (\\d+)$")
+    public void thereIsTakeAvailableWithId(Long id) {
+        Assert.assertEquals("Take with this id", this.takeRepository.findById(id), Optional.empty());
     }
 }
