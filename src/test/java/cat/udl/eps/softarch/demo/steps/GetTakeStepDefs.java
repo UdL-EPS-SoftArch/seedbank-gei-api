89b1d0ad23a09bfa02a314749e67045d9431dcea
Oscar <oscar13785@gmail.com>
2023-03-01 18:10:26 +0100

    Finishes GetTakeStepDefs

--- src/test/java/cat/udl/eps/softarch/demo/steps/GetTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/GetTakeStepDefs.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 public class GetTakeStepDefs {
@@ -33,5 +35,12 @@ public class GetTakeStepDefs {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(stepDefs.mapper.writeValueAsString(take))
         );
+    }
+
+    @And("^I get a new Take with id (\\d+)$")
+    public void takeHasBeenCreated(Long id) throws Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/takes/{id}", id)
+                        .accept(MediaType.APPLICATION_JSON));
     }
 }
