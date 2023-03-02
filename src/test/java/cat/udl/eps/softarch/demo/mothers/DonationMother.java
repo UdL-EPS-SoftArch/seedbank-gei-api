a417e8f2ed0451b2aa0dac0415941f7acfc2fd4c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-03-02 12:31:06 +0100

    test: make 'create donation' feature use mothers

--- src/test/java/cat/udl/eps/softarch/demo/mothers/DonationMother.java
+++ src/test/java/cat/udl/eps/softarch/demo/mothers/DonationMother.java
@@ -3,7 +3,15 @@ package cat.udl.eps.softarch.demo.mothers;
 import cat.udl.eps.softarch.demo.domain.Donation;
 import cat.udl.eps.softarch.demo.domain.Donor;
 import cat.udl.eps.softarch.demo.domain.Take;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
@@ -15,7 +23,17 @@ public class DonationMother {
         donation.setTakenBy(take);
         donation.setAmount(1);
         donation.setWeight(BigDecimal.ONE);
+        donation.setLocation("Barcelona");
         donation.setDate(ZonedDateTime.now());
         return donation;
     }
+
+    public static Donation getDonationOnlyWith(Donor donor) {
+        return DonationMother.getValidDonationFor(donor, null);
+    }
+
+    public static Donation getDonationOnlyWith(Take take) {
+        return DonationMother.getValidDonationFor(null, take);
+    }
+
 }
--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateDonationStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateDonationStepDefs.java
@@ -1,21 +1,19 @@
 package cat.udl.eps.softarch.demo.steps;
 
-import cat.udl.eps.softarch.demo.domain.Donation;
 import cat.udl.eps.softarch.demo.domain.Donor;
-import cat.udl.eps.softarch.demo.domain.Take;
 import cat.udl.eps.softarch.demo.domain.User;
+import cat.udl.eps.softarch.demo.mothers.DonationMother;
+import cat.udl.eps.softarch.demo.mothers.DonorMother;
+import cat.udl.eps.softarch.demo.mothers.TakeMother;
 import cat.udl.eps.softarch.demo.repository.DonationRepository;
+import cat.udl.eps.softarch.demo.repository.DonorRepository;
 import cat.udl.eps.softarch.demo.repository.TakeRepository;
-import cat.udl.eps.softarch.demo.repository.UserRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.But;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,44 +24,38 @@ public class CreateDonationStepDefs {
     @Autowired
     private StepDefs stepDefs;
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
     private TakeRepository takeRepository;
     @Autowired
     private DonationRepository donationRepository;
 
-    @And("There is a valid Donor")
-    public void thereIsAValidDonor() {
-        Donor donor = createValidDonor();
-        userRepository.save(donor);
-        Assert.assertNotEquals(0, userRepository.count());
+    @Autowired
+    private DonorRepository donorRepository;
+
+    @And("There is a valid Donor with name {string}")
+    public void thereIsAValidDonor(String name) {
+        Donor donor = DonorMother.getValidDonorWithName(name);
+        donorRepository.save(donor);
     }
 
-    @And("There is a valid Take")
-    public void thereIsAValidTake() {
-        Take take = createValidTake();
+    @And("There is a valid Take with identifier {long}")
+    public void thereIsAValidTake(long takeId) {
+        var take = TakeMother.getValidTake();
         takeRepository.save(take);
-        Assert.assertNotEquals(0, takeRepository.count());
     }
 
-    @When("I create a new valid donation")
-    public void createDonation() throws Exception {
-        Donation donation = createValidDonation();
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                post("/donations")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(stepDefs.mapper.writeValueAsString(donation))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
+    @When("I create a new valid donation with id: {long} from donor {string} and take {long}")
+    public void createDonation(long id, String donorName, long takeId) throws Exception {
+        var donor = donorRepository.findById(donorName).orElseThrow();
+        var take = takeRepository.findById(takeId).orElseThrow();
+        var donation = DonationMother.getValidDonationFor(donor, take);
+        stepDefs.result = stepDefs.mockMvc.perform(post("/donations").contentType(MediaType.APPLICATION_JSON_VALUE).characterEncoding("utf-8").content(stepDefs.mapper.writeValueAsString(donation)).accept(MediaType.APPLICATION_JSON).with(AuthenticationStepDefs.authenticate())).andDo(print());
     }
 
     @But("There is no Donor")
     public void thereIsNoValidDonor() {
-        for (User user : userRepository.findAll()) {
+        // TODO: refactor this
+        for (User user : donorRepository.findAll())
             Assert.assertFalse(user instanceof Donor);
-        }
     }
 
     @But("There is no Take")
@@ -76,84 +68,18 @@ public class CreateDonationStepDefs {
         Assert.assertEquals(numDonations, donationRepository.count());
     }
 
-    @When("I create a new donation without a donor")
-    public void createDonationWithoutDonor() throws Exception {
-        Donation donation = donationWithoutDonor();
-        donation.setDonor(null);
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                post("/donations")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(stepDefs.mapper.writeValueAsString(donation))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .characterEncoding("utf-8")
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
+    @When("I create a new donation without a donor and a take with id {long}")
+    public void createDonationWithoutDonor(long takeId) throws Exception {
+        var take = takeRepository.findById(takeId).orElseThrow();
+        var donation = DonationMother.getDonationOnlyWith(take);
+        stepDefs.result = stepDefs.mockMvc.perform(post("/donations").contentType(MediaType.APPLICATION_JSON).content(stepDefs.mapper.writeValueAsString(donation)).accept(MediaType.APPLICATION_JSON).characterEncoding("utf-8").with(AuthenticationStepDefs.authenticate())).andDo(print());
     }
 
-    @When("I create a new donation without a take")
-    public void createDonationWithoutTake() throws Exception {
-        Donation donation = donationWithoutTake();
-        donation.setTakenBy(null);
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                        post("/donations")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(stepDefs.mapper.writeValueAsString(donation))
-                                .accept(MediaType.APPLICATION_JSON)
-                                .characterEncoding("utf-8")
-                                .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
+    @When("I create a new donation with id {long} without a take and with a donor name {string}")
+    public void createDonationWithoutTake(long id, String name) throws Exception {
+        var donor = donorRepository.findById(name).orElseThrow();
+        var donation = DonationMother.getDonationOnlyWith(donor);
+        stepDefs.result = stepDefs.mockMvc.perform(post("/donations").contentType(MediaType.APPLICATION_JSON).content(stepDefs.mapper.writeValueAsString(donation)).accept(MediaType.APPLICATION_JSON).characterEncoding("utf-8").with(AuthenticationStepDefs.authenticate())).andDo(print());
     }
 
-
-    private Donor createValidDonor() {
-        Donor donor = new Donor();
-        donor.setUsername("iAmADonor");
-        donor.setPassword("donorPassword");
-        donor.encodePassword();
-        donor.setEmail("donor@donor.donor");
-        return donor;
-    }
-
-    private Take createValidTake() {
-        Take take = new Take();
-        take.setAmount(6);
-        take.setWeight(BigDecimal.TEN);
-        take.setLocation("Barcelona");
-        take.setDate(ZonedDateTime.now());
-        return take;
-    }
-
-    private Donation createValidDonation() {
-        Donation donation = new Donation();
-        donation.setDonor(createValidDonor());
-        donation.setTakenBy(createValidTake());
-        donation.setAmount(1);
-        donation.setWeight(BigDecimal.ONE);
-        donation.setDate(ZonedDateTime.now());
-        return donation;
-    }
-
-    private Donation donationWithoutDonor() {
-        Donation donation = new Donation();
-        donation.setDonor(null);
-        donation.setTakenBy(createValidTake());
-        donation.setAmount(1);
-        donation.setWeight(BigDecimal.ONE);
-        donation.setLocation("Barcelona");
-        donation.setDate(ZonedDateTime.now());
-        return donation;
-    }
-
-    private Donation donationWithoutTake() {
-        Donation donation = new Donation();
-        donation.setDonor(createValidDonor());
-        donation.setTakenBy(null);
-        donation.setAmount(1);
-        donation.setWeight(BigDecimal.ONE);
-        donation.setLocation("Barcelona");
-        donation.setDate(ZonedDateTime.now());
-        return donation;
-    }
 }
