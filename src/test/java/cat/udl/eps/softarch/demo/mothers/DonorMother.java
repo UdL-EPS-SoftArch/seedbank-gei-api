d207c49b1286a93028627eeb56c1b80b0e756d7f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-03-02 15:28:14 +0100

    test(mothers): add annotations and clean unused methods

--- src/test/java/cat/udl/eps/softarch/demo/mothers/DonorMother.java
+++ src/test/java/cat/udl/eps/softarch/demo/mothers/DonorMother.java
@@ -1,35 +1,16 @@
 package cat.udl.eps.softarch.demo.mothers;
 
 import cat.udl.eps.softarch.demo.domain.Donor;
+import org.jetbrains.annotations.Nullable;
 
 public class DonorMother {
-    public static final String ValidDonorUsername = "iAmADonor";
-    private static final String ValidDonorPassword = "donorPassword";
-    private static final String ValidDonorEmail = "donor@donor.donor";
-
-    public static Donor getValidDonorWithNameAndPassword(String name, String password) {
-        return getValidDonorWith(name, password, ValidDonorEmail);
-    }
-
-    public static Donor getValidDonorWith(String name, String password, String email) {
+    public static Donor getValidDonorWith(@Nullable String name, @Nullable String password, @Nullable String email) {
         Donor donor = new Donor();
         donor.setUsername(name);
         donor.setPassword(password);
         donor.setEmail(email);
         donor.encodePassword();
         return donor;
-    }
-
-    public static Donor getValidDonorWithName(String name) {
-        return getValidDonorWithNameAndPassword(name, ValidDonorPassword);
-    }
-
-    public static Donor getValidDonorWithPassword(String password) {
-        return getValidDonorWithNameAndPassword(ValidDonorUsername, password);
-    }
-
-    public static Donor getValidDonor() {
-        return getValidDonorWithNameAndPassword(ValidDonorUsername, ValidDonorPassword);
     }
 
 }
