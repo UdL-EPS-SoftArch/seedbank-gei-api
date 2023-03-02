5ef501e58441f38971f544581be6d9b5c282c9e8
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-03-02 15:28:14 +0100

    test(mothers): add annotations and clean unused methods

--- src/test/java/cat/udl/eps/softarch/demo/mothers/DonationMother.java
+++ src/test/java/cat/udl/eps/softarch/demo/mothers/DonationMother.java
@@ -3,13 +3,14 @@ package cat.udl.eps.softarch.demo.mothers;
 import cat.udl.eps.softarch.demo.domain.Donation;
 import cat.udl.eps.softarch.demo.domain.Donor;
 import cat.udl.eps.softarch.demo.domain.Take;
+import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
 public class DonationMother {
 
-    public static Donation getValidDonationFor(Donor donor, Take take) {
+    public static Donation getValidDonationFor(@Nullable Donor donor, @Nullable Take take) {
         Donation donation = new Donation();
         donation.setDonor(donor);
         donation.setTakenBy(take);
@@ -18,14 +19,6 @@ public class DonationMother {
         donation.setLocation("Barcelona");
         donation.setDate(ZonedDateTime.now());
         return donation;
-    }
-
-    public static Donation getDonationOnlyWith(Donor donor) {
-        return DonationMother.getValidDonationFor(donor, null);
-    }
-
-    public static Donation getDonationOnlyWith(Take take) {
-        return DonationMother.getValidDonationFor(null, take);
     }
 
 }
