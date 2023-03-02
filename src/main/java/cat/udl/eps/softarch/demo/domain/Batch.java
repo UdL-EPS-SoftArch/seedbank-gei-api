21da0cedb05abd71cf71cdb464e920b7f86c31c5
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-03-02 12:23:45 +0100

    fix: batch id type and make it an url entity
    Come on, we're at 4th grade, we should know the differences between a
    long and Long (and, if we use frameworks that do some Reflection
    programming under the hood, we should use allways reference types, not
    primitive ones, since some cases don't do the wrapping and unwrapping
    by default).
    
    About making it an url entity: As a user of the API I think
    that creating a batch from the frontend makes sense.
    
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Batch.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -11,10 +12,11 @@ import java.time.ZonedDateTime;
 
 @Entity
 @Data
-public class Batch {
+@EqualsAndHashCode(callSuper = true)
+public class Batch extends UriEntity<Long> {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private long id;
+    private Long id;
 
 
     @NotNull
@@ -31,6 +33,4 @@ public class Batch {
 
     private ZonedDateTime date = ZonedDateTime.now();
 
-    // @ManyToOne
-    // private Seed of;
 }
