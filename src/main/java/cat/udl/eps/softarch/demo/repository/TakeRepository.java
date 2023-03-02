5f5eb8db1189c6a6d992569d04add2d64a17c14c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-03-02 12:22:03 +0100

    fix: take repository identifier
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/TakeRepository.java
@@ -4,7 +4,7 @@ import cat.udl.eps.softarch.demo.domain.Take;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface TakeRepository extends PagingAndSortingRepository<Take, String> {
+public interface TakeRepository extends PagingAndSortingRepository<Take, Long> {
 
     //This is not void, when merge uncomment Batch
     //Take findTakeFulfilledByBatchId(@Param("id") Integer batchId);
