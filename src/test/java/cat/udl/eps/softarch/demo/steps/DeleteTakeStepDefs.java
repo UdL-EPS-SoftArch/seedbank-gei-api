d52a8d91bb1eb45d0a04771046ef1f8dc6a32f08
MarioMontanuy <mariobnf58@gmail.com>
2023-03-07 18:33:10 +0100

    Adds new scenario "Delete a Take with user not logged in"

--- src/test/java/cat/udl/eps/softarch/demo/steps/DeleteTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/DeleteTakeStepDefs.java
@@ -1,15 +1,32 @@
 package cat.udl.eps.softarch.demo.steps;
 
+import cat.udl.eps.softarch.demo.domain.Take;
+import cat.udl.eps.softarch.demo.repository.TakeRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 public class DeleteTakeStepDefs {
     @Autowired
     private StepDefs stepDefs;
+    @Autowired
+    private TakeRepository takeRepository;
+
+    @And("There is a Take created with amount {int}, weight {int} and location {string}")
+    public void thereIsATakeCreatedWithAmountWeightAndLocation(int amount, int weight, String location) {
+        Take take = new Take();
+        take.setAmount(amount);
+        take.setWeight(new BigDecimal(weight));
+        take.setLocation(location);
+        takeRepository.save(take);
+        take = takeRepository.findAll().iterator().next();
+        CreateTakeStepDefs.newResourceUri = "/takes/" + take.getId();
+    }
     @When("^I delete Take$")
     public void iDeleteATake() throws Throwable {
         stepDefs.result = stepDefs.mockMvc.perform(
@@ -26,4 +43,5 @@ public class DeleteTakeStepDefs {
                         .with(AuthenticationStepDefs.authenticate())
         );
     }
+
 }
--- src/test/resources/features/DeleteTake.feature
+++ src/test/resources/features/DeleteTake.feature
@@ -5,10 +5,16 @@ I want to delete a Take
   Background:
     Given There is a registered user with username "username" and password "password" and email "user@sample.app"
 
-  Scenario: Delete a Take with user
+  Scenario: Delete a Take with user logged in
     Given I login as "username" with password "password"
     And I create a new Take with amount 5, weight 5 and location "Lleida"
     When I delete Take
     Then The response code is 204
     And Take has been deleted
     And The response code is 404
+
+  Scenario: Delete a Take with user not logged in
+    Given I'm not logged in
+    And There is a Take created with amount 5, weight 5 and location "Lleida"
+    When I delete Take
+    Then The response code is 401
