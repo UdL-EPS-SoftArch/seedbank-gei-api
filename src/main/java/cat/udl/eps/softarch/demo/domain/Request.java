b439b345f2f92f09f214540961e34618d4914f25
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-03-08 12:16:26 +0100

    feat: add needed decorators on request class
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/domain/Request.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Request.java
@@ -1,15 +1,24 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Request extends Batch{
+    @ManyToOne
+    @NotNull
+    @JsonIdentityReference(alwaysAsId = true)
     private Propagator propagator;
 
+    @OneToOne
+    @JsonIdentityReference(alwaysAsId = true)
     private Take fulfilledBy;
 }
