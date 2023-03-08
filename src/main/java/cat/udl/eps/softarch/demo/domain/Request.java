b7292da67a66963955fe390684a9f3401588325b
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-03-08 12:15:38 +0100

    feat: add request skeleton
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Request.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Request extends Batch{
+}
