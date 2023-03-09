a8c3efbc3c5d89f236a54f81523b6ccee0e1542e
MarioMontanuy <mariobnf58@gmail.com>
2023-03-09 16:24:12 +0100

    Adds Propagator Attributes and modifies PropagatorMother

--- src/main/java/cat/udl/eps/softarch/demo/domain/Propagator.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Propagator.java
@@ -3,10 +3,16 @@ package cat.udl.eps.softarch.demo.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import java.util.ArrayList;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Propagator extends User {
+    @ElementCollection
+    @CollectionTable(name = "listOfTakes")
+    private ArrayList<Take> takes = new ArrayList<>();
 }
--- src/test/java/cat/udl/eps/softarch/demo/mothers/PropagatorMother.java
+++ src/test/java/cat/udl/eps/softarch/demo/mothers/PropagatorMother.java
@@ -1,22 +1,27 @@
 package cat.udl.eps.softarch.demo.mothers;
 
 import cat.udl.eps.softarch.demo.domain.Propagator;
+import cat.udl.eps.softarch.demo.domain.Take;
 import org.jetbrains.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PropagatorMother {
     private static final String VALID_PASSWORD = "password";
     private static final String VALID_EMAIL = "propagator@sample.com";
+    private static final ArrayList<Take> VALID_LIST_OF_TAKES = new ArrayList<>(List.of(TakeMother.getValidTake()));
 
-    public static Propagator getValidPropagatorWith(@Nullable String name, @Nullable String password, @Nullable String email) {
+    public static Propagator getValidPropagatorWith(@Nullable String name, @Nullable String password, @Nullable String email, @Nullable ArrayList<Take> listOfTakes) {
         var propagator = new Propagator();
         propagator.setUsername(name);
         propagator.setPassword(password);
         propagator.setEmail(email);
         propagator.encodePassword();
+        propagator.setTakes(listOfTakes);
         return propagator;
     }
 
     public static Propagator getValidPropagatorWith(@Nullable String name) {
-        return getValidPropagatorWith(name, VALID_PASSWORD, VALID_EMAIL);
+        return getValidPropagatorWith(name, VALID_PASSWORD, VALID_EMAIL, VALID_LIST_OF_TAKES);
     }
 }
