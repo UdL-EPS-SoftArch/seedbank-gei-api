c82912c574debcb561d1ee2a405660b787c83364
sys-walker <pere.rollon@gmail.com>
2023-03-16 16:03:48 +0100

    update test

--- src/test/java/cat/udl/eps/softarch/demo/steps/UpdateTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/UpdateTakeStepDefs.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -23,19 +24,40 @@ public class UpdateTakeStepDefs {
     @Autowired
     private StepDefs stepDefs;
 
+    private static ResultActions updatedTaskResult;
+
 
     @When("^I update Take with changing amount to (\\d+)$")
     public void iUpdateTakeWithChangingAmountTo(Integer newAmount)throws Throwable{
         JSONObject modifyTake = new JSONObject();
         modifyTake.put("amount", newAmount);
         stepDefs.result = stepDefs.mockMvc.perform(patch(CreateTakeStepDefs.newResourceUri)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(modifyTake.toString())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(modifyTake.toString())
+                .accept(MediaType.APPLICATION_JSON)
+                .with(AuthenticationStepDefs.authenticate()));
+        updatedTaskResult = stepDefs.result;
 
-        JSONObject updateTakeJSON = new JSONObject(stepDefs.result.andReturn().getResponse().getContentAsString());
-        Assert.assertEquals(newAmount, updateTakeJSON.get("amount"));
     }
-    
+
+    @And("^I check if the new amount is not (\\d+)$")
+    public void iCheckIfTheNewAmountIsNot(Integer oldAmount) throws Throwable{
+        JSONObject updatedTakeJSON = new JSONObject(updatedTaskResult.andReturn().getResponse().getContentAsString());
+        Assert.assertNotEquals(oldAmount, updatedTakeJSON.get("amount"));
+    }
+
+    @When("I update Take with id (\\d+) changing amount to (\\d+)$")
+    public void iUpdateTakeWithIdChangingAmountTo(Long id, Integer newAmount) throws Throwable{
+        ///takes/1
+        JSONObject modifyTake = new JSONObject();
+        modifyTake.put("amount", newAmount);
+
+        System.out.println(CreateTakeStepDefs.newResourceUri);
+        stepDefs.result = stepDefs.mockMvc.perform(patch( "/takes" + id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(modifyTake.toString())
+                .accept(MediaType.APPLICATION_JSON)
+                .with(AuthenticationStepDefs.authenticate()));
+        updatedTaskResult = stepDefs.result;
+    }
 }
--- src/test/resources/features/UpdateTake.feature
+++ src/test/resources/features/UpdateTake.feature
@@ -1,6 +1,6 @@
 Feature: Update Take
-In order to update Take
-I want to update a new Take
+  In order to update Take
+  I want to update a new Take
 
   Background:
     Given There is a registered user with username "username" and password "password" and email "user@sample.app"
@@ -9,3 +9,20 @@ I want to update a new Take
     And I create a new Take with amount 5, weight 5 and location "Lleida"
     When I update Take with changing amount to 24
     Then The response code is 200
+    And I check if the new amount is not 5
+
+  Scenario: Update a Take with user not logged in
+    Given I'm not logged in
+    And There is a Take created with amount 5, weight 5 and location "Lleida"
+    When I update Take with changing amount to 24
+    Then The response code is 401
+
+  Scenario: Update an inexistent Take with user
+    Given I login as "username" with password "password"
+    When I update Take with id 1000 changing amount to 24
+    Then The response code is 404
+
+  Scenario: Update a Take with user not logged in
+    Given I'm not logged in
+    When I update Take with id 1000 changing amount to 24
+    Then The response code is 401
