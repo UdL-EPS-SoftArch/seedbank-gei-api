cd49887cc12c0e8579b8e9c3e1971de3e5d63c0a
Oscar <oscar13785@gmail.com>
2023-03-23 17:08:26 +0100

    Solves pere tests to make the app work
    Co-authored-by: MarioMontanuy <mariobnf58@gmail.com>

--- src/test/java/cat/udl/eps/softarch/demo/steps/UpdateTakeStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/UpdateTakeStepDefs.java
@@ -6,12 +6,14 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.sk.Tak;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -23,9 +25,12 @@ public class UpdateTakeStepDefs {
     @Autowired
     private StepDefs stepDefs;
 
+    private int newAmount;
 
-    @When("^I update Take with changing amount to (\\d+)$")
-    public void iUpdateTakeWithChangingAmountTo(Integer newAmount)throws Throwable{
+
+    @When("^I update Take changing amount to (\\d+)$")
+    public void iUpdateTakeChangingAmountTo(int newAmount) throws Throwable {
+        this.newAmount = newAmount;
         JSONObject modifyTake = new JSONObject();
         modifyTake.put("amount", newAmount);
         stepDefs.result = stepDefs.mockMvc.perform(patch(CreateTakeStepDefs.newResourceUri)
@@ -33,9 +38,18 @@ public class UpdateTakeStepDefs {
                         .content(modifyTake.toString())
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()));
+    }
 
+    @And("^I check that Take has been succesfully updated")
+    public void iCheckThatTakeHasBeenSuccesfullyUpdated() throws Throwable {
         JSONObject updateTakeJSON = new JSONObject(stepDefs.result.andReturn().getResponse().getContentAsString());
         Assert.assertEquals(newAmount, updateTakeJSON.get("amount"));
+    }
+
+    @When("^I update Take with id (\\d+) changing amount to (\\d+)")
+    public void iUpdateTakeWithIdChangingAmountTo(int id, int amount) throws Throwable {
+        CreateTakeStepDefs.newResourceUri = "/takes/" + id;
+        iUpdateTakeChangingAmountTo(amount);
     }
     
 }
