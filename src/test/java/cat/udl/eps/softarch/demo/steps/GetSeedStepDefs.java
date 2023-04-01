8936b4a44c5a70306ca13fe76a6e91f4060a05c9
RamoHasanovic <hasanovic.ramo8@gmail.com>
2023-04-01 04:37:42 +0200

    merge with create seed branch

---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/GetSeedStepDefs.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import io.cucumber.java.en.And;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GetSeedStepDefs {
+
+
+    private StepDefs stepDefs;
+
+
+
+}
