2558355ec6c72a05d021daa4e2e23729bf7bc015
Roberto García <roberto@rhizomik.net>
2023-04-26 17:27:43 +0200

    Undo test config changes

--- src/test/java/cat/udl/eps/softarch/demo/DemoApplicationTests.java
+++ src/test/java/cat/udl/eps/softarch/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package cat.udl.eps.softarch.demo;
 
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestPropertySource(locations = "classpath:application-test.yml")
 public class DemoApplicationTests {
 
     @Test
