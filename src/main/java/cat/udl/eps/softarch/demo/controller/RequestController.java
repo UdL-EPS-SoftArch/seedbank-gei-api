bcc2e2f0a00560763e853e8fb5ae74239c385177
FerranAD <ferranaran@tuta.io>
2023-05-04 20:35:54 +0200

    feat(check-user-role): fix controller
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
+++ src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
@@ -1,46 +1,31 @@
 package cat.udl.eps.softarch.demo.controller;
 
-import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.Request;
-import cat.udl.eps.softarch.demo.mothers.RequestMother;
 import cat.udl.eps.softarch.demo.repository.RequestRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 @BasePathAwareController
 public class RequestController {
 
-    private RequestRepository requestRepository;
+    private final RequestRepository requestRepository;
 
-    public RequestCOntroller(
+    public RequestController(RequestRepository requestRepository) {
         this.requestRepository = requestRepository;
-    )
-
-//    @PostMapping("/requests")
-//    public @ResponseBody ResponseEntity<Request> createRequest(HttpServletRequest rawRequest) throws IOException {
-//        String requestBody = rawRequest.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode rootNode = mapper.readTree(requestBody);
-//        JsonNode propagatorNode = rootNode.get("propagator");
-//        Request request = mapper.readValue(requestBody, Request.class);
-//        Request savedRequest = requestRepository.save(request);
-//        return ResponseEntity.ok(savedRequest);
-//    }
+    }
 
     @PostMapping("/requests")
     public @ResponseBody ResponseEntity<Request> createRequest(@RequestBody Request request) {
-        Request savedRequest = requestRepository.save(request);
+        Request newRequest = new Request();
+        newRequest.setPropagator(request.getPropagator());
+        newRequest.setDate(request.getDate());
+        newRequest.setWeight(request.getWeight());
+        newRequest.setAmount(request.getAmount());
+        newRequest.setLocation(request.getLocation());
+        Request savedRequest = requestRepository.save(newRequest);
         return ResponseEntity.ok(savedRequest);
     }
 }
