6632d4433f309c0c83d89fd5dcf4330cca040687
FerranAD <ferranaran@tuta.io>
2023-05-04 16:11:49 +0200

    feat(check-user-role): add request repository
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
+++ src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
@@ -1,16 +1,28 @@
 package cat.udl.eps.softarch.demo.controller;
 
+import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.Request;
+import cat.udl.eps.softarch.demo.mothers.RequestMother;
+import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 
 @BasePathAwareController
 public class RequestController {
 
+    @Autowired
+    private RequestRepository requestRepository;
+
     @PostMapping("/requests")
-    public @ResponseBody ResponseEntity<Request> createRequest() {
-        return null;
+    public @ResponseBody ResponseEntity<Request> createRequest(HttpServletRequest request) {
+        Request savedRequest = requestRepository.save(request);
+        return ResponseEntity.ok(savedRequest);
     }
 }
