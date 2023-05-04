0a1c2771dac2e14bc090573047a2a692c00c8c84
FerranAD <ferranaran@tuta.io>
2023-05-04 20:37:14 +0200

    feat(check-user-role): add entity resolver on propagator
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/domain/Propagator.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Propagator.java
@@ -1,18 +1,26 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import cat.udl.eps.softarch.demo.resolver.EntityIdResolver;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
 import java.util.Collection;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id",
+        resolver = EntityIdResolver.class,
+        scope = Propagator.class)
 public class Propagator extends User {
     @Override
     @JsonValue(value = false)
