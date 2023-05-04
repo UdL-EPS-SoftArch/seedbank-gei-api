c017bc956b970eaf64e30bc730cebd729bff7cec
FerranAD <ferranaran@tuta.io>
2023-05-04 16:38:55 +0200

    feat(check-user-role): obtain raw json to check propagator role
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
+++ src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
@@ -4,6 +4,8 @@ import cat.udl.eps.softarch.demo.domain.Propagator;
 import cat.udl.eps.softarch.demo.domain.Request;
 import cat.udl.eps.softarch.demo.mothers.RequestMother;
 import cat.udl.eps.softarch.demo.repository.RequestRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
+import java.io.IOException;
+import java.util.stream.Collectors;
 
 @BasePathAwareController
 public class RequestController {
@@ -21,7 +24,12 @@ public class RequestController {
     private RequestRepository requestRepository;
 
     @PostMapping("/requests")
-    public @ResponseBody ResponseEntity<Request> createRequest(HttpServletRequest request) {
+    public @ResponseBody ResponseEntity<Request> createRequest(HttpServletRequest rawRequest) throws IOException {
+        String requestBody = rawRequest.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(requestBody);
+        JsonNode propagatorNode = rootNode.get("propagator");
+        Request request = mapper.readValue(requestBody, Request.class);
         Request savedRequest = requestRepository.save(request);
         return ResponseEntity.ok(savedRequest);
     }
