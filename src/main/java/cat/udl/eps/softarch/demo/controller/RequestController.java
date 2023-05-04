3da48b6db66daaea98ad7e06b9336bdc28ce7aba
FerranAD <ferranaran@tuta.io>
2023-05-04 15:30:58 +0200

    feat(check-user-role): add skeleton for post mapping
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
+++ src/main/java/cat/udl/eps/softarch/demo/controller/RequestController.java
@@ -1,7 +1,16 @@
 package cat.udl.eps.softarch.demo.controller;
 
+import cat.udl.eps.softarch.demo.domain.Request;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @BasePathAwareController
 public class RequestController {
+
+    @PostMapping("/requests")
+    public @ResponseBody ResponseEntity<Request> createRequest() {
+        return null;
+    }
 }
