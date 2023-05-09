2cc4993573370ea341f9eeafd548abfb0f2da3f8
Oriol Agost Batalla <gpg18@agost.info>
2023-05-09 12:41:36 +0200

    refactor: change exception's return status code
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/exceptions/WrongUserRoleException.java
+++ src/main/java/cat/udl/eps/softarch/demo/exceptions/WrongUserRoleException.java
@@ -1,5 +1,9 @@
 package cat.udl.eps.softarch.demo.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "El usuario debe ser de tipo propagator")
 public class WrongUserRoleException extends RuntimeException {
     public WrongUserRoleException(String message) {
         super(message);
