f98b225c0a6e290e56543b55c4fe6b220136fdfe
MarioMontanuy <mariobnf58@gmail.com>
2023-05-25 16:11:27 +0200

    Adds Take before creating donation
    Co-authored-by: Oscars35 <oscar13785@gmail.com>
    Co-authored-by: sys-walker <pere.rollon@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/repository/RequestRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/RequestRepository.java
@@ -1,14 +1,18 @@
 package cat.udl.eps.softarch.demo.repository;
 
+import cat.udl.eps.softarch.demo.domain.Batch;
 import cat.udl.eps.softarch.demo.domain.Request;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.List;
 
 @RepositoryRestResource
 public interface RequestRepository extends PagingAndSortingRepository<Request, Long> {
+
+    Request findRequestByAmountAndWeight(Integer amount, BigDecimal weight);
 }
