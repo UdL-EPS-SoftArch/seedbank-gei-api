b6896520376a51705bdf310f8343cdeb6c8a5d06
patrickpelegrina1 <patrickpelegrinacuen@gmail.com>
2023-06-08 21:33:12 +0200

    Search Donations Co-authored-by: JoelGuitartCarvajal <joelguitartcarvajal@gmail.com>

--- src/main/java/cat/udl/eps/softarch/demo/repository/DonationRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/DonationRepository.java
@@ -2,8 +2,12 @@ package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Donation;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface DonationRepository extends PagingAndSortingRepository<Donation, Long> {
+    List<Donation> findByLocation(@Param("text") String text);
 }
